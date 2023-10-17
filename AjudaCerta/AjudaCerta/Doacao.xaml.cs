8db26152c15571a73df89f75a572c75e0d4543b8
ViniciusMateos <vinimateos.s@gmail.com>
2023-10-17 20:35:53 -0300

    Tela de doação finalizada

---
+++ AjudaCerta/AjudaCerta/Doacao.xaml.cs
@@ -0,0 +1,9 @@
+namespace AjudaCerta;
+
+public partial class Doacao : ContentPage
+{
+	public Doacao()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AjudaCerta/AjudaCerta/Views/Doacao.xaml.cs
@@ -0,0 +1,19 @@
+namespace AjudaCerta.Views;
+
+public partial class Doacao : ContentPage
+{
+	public Doacao()
+	{
+		InitializeComponent();
+	}
+
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Entry_DescendantAdded(object sender, ElementEventArgs e)
+    {
+
+    }
+}
